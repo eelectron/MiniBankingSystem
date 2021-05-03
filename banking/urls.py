1ae68cb0276cca07c085476f950cc708af3311bc
Prashant Singh <psonlinux@gmail.com>
2021-05-03 22:38:10 +0530

    create urls

---
+++ banking/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+	path("", views.index, name="index"),
+	path("login/", views.login, name="login"),
+	path("logout/", views.logout, name="logout"),
+	path("register/", views.register, name="register"),
+	path("addAddress/", views.addAddress, name="address"),
+	path("createAccount/", views.createAccount, name="account"),
+	path("transfer/", views.transfer, name="transfer"),
+]
--- banking/views.py
+++ banking/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+'''
+Show the main page
+'''
+def index(request):
+	return render(request, "banking/index.html")
+
+
+'''
+	Autheticate the user .
+	Logs a user inside website .  
+'''
+def login(request):
+	if request.method == "POST":
+		print("login")
+	else:
+		return render(request, "banking/login.html")
+
+'''
+	Logs a user out of website
+'''
+def logout(request):
+	print("logout")
+	return render(request, "banking/login.html")
+
+'''
+	Registers a user
+'''
+def register(request):
+	if request.method == "POST":
+		print("register")
+	else:
+		return render(request, "banking/register.html")
+
+'''
+	Add address of customer
+'''
+def addAddress(request):
+	print("address")
+
+'''
+	Create a account for customer .
+	For ex: Savig, Current, Fixed
+'''
+def createAccount(request):
+	print("account")
+
+'''
+	Transfer amount from one account to 
+	other 
+'''
+def transfer(request):
+	print("transfer")
